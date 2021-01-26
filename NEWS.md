66176c79be277f0912baee4a3441753ef9967ae5
Igor Dolgalev <igor.dolgalev@nyumc.org>
2021-01-26 05:40:02 +0000

    version 1.1.0

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,9 @@
+# clustermole 1.1.0
+
+* Added singscore enrichment method.
+* Added combined enrichment method.
+* Updated cell type markers.
+
 # clustermole 1.0.1
 
 * Updated documentation.
--- inst/doc/clustermole-intro.R
+++ inst/doc/clustermole-intro.R
@@ -3,6 +3,8 @@ knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
+# reduce the minimum number of characters for the tibble column titles (default: 15)
+options(pillar.min_title_chars = 10)
 
 ## ----install-package, eval=FALSE----------------------------------------------
 #  install.packages("clustermole")
@@ -10,37 +12,22 @@ knitr::opts_chunk$set(
 ## ----load-package, message=FALSE----------------------------------------------
 library(clustermole)
 
-## ----overlaps-----------------------------------------------------------------
-my_genes = c("CD2", "CD3D", "CD3E", "IL7R", "IL32", "LTB", "LDHB", "CCR7")
-my_overlaps = clustermole_overlaps(genes = my_genes, species = "hs")
-my_overlaps
-
-## ----enrichment, eval=FALSE---------------------------------------------------
-#  clustermole_enrichment(expr_mat = my_expr_mat, species = "hs")
-
 ## ----markers------------------------------------------------------------------
-markers = clustermole_markers(species = "hs")
+markers <- clustermole_markers(species = "hs")
 markers
 
 ## ----celltypes-list-----------------------------------------------------------
-markers_list = split(x = markers$gene, f = markers$celltype_full)
+markers_list <- split(x = markers$gene, f = markers$celltype_full)
 
-## ----load-dplyr, message=FALSE------------------------------------------------
-library(dplyr)
+## ----celltypes-count----------------------------------------------------------
+length(unique(markers$celltype_full))
 
-## ----get-markers--------------------------------------------------------------
-markers = clustermole_markers(species = "hs")
-markers
+## ----celltypes-dbs------------------------------------------------------------
+sort(unique(markers$db))
 
-## ----celltypes-length---------------------------------------------------------
-markers %>% distinct(celltype_full) %>% nrow()
+## ----overlaps, eval=FALSE-----------------------------------------------------
+#  my_overlaps <- clustermole_overlaps(genes = my_genes_vec, species = "hs")
 
-## ----count-species------------------------------------------------------------
-markers %>% distinct(celltype_full, species) %>% count(species, sort = TRUE)
-
-## ----count-organ--------------------------------------------------------------
-markers %>% distinct(celltype_full, organ) %>% count(organ, sort = TRUE)
-
-## ----package-version----------------------------------------------------------
-packageVersion("clustermole")
+## ----enrichment, eval=FALSE---------------------------------------------------
+#  my_enrichment <- clustermole_enrichment(expr_mat = my_expr_mat, species = "hs")
 
